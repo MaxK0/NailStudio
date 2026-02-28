dc872d5ee338680d54f02c36bd6da67c557c6323
MaxK0 <maxkoryakov1@gmail.com>
2026-03-01 00:41:08 +0500

    Fix

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -9,36 +9,37 @@
     @stack('styles')
 </head>
 <body>
-<header>
-    <div class="container">
-        <div class="logo">
-            <img src="{{ asset('img/kkk_log.png') }}" width="45" alt="" />
-            <h1>Nail<span>Studio</span></h1>
+<div id="site">
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="{{ asset('img/kkk_log.png') }}" width="45" alt="" />
+                <h1>Nail<span>Studio</span></h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="{{ route('home') }}">Главная</a></li>
+                    <li><a href="/#services">Услуги</a></li>
+                    <li><a href="/#about">О нас</a></li>
+                    <li><a href="/#contact">Контакты</a></li>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul>
-                <li><a href="{{ route('home') }}">Главная</a></li>
-                <li><a href="/#services">Услуги</a></li>
-                <li><a href="/#about">О нас</a></li>
-                <li><a href="/#contact">Контакты</a></li>
-            </ul>
-        </nav>
-    </div>
-</header>
+    </header>
 
-@yield('content')
+    @yield('content')
 
-<footer>
-    <div class="container">
-        <p>&copy; 2025 NailStudio. Все права защищены.</p>
-        <div class="social">
-            <a href="#"><i class="fab fa-instagram"></i></a>
-            <a href="#"><i class="fab fa-vk"></i></a>
-            <a href="#"><i class="fab fa-whatsapp"></i></a>
+    <footer>
+        <div class="container">
+            <p>&copy; 2025 NailStudio. Все права защищены.</p>
+            <div class="social">
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-vk"></i></a>
+                <a href="#"><i class="fab fa-whatsapp"></i></a>
+            </div>
         </div>
-    </div>
-</footer>
-
+    </footer>
+</div>
 <script src="{{ asset('js/script.js') }}"></script>
 @stack('scripts')
 </body>
